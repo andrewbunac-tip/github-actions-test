84351d34f7dfd654791261b688a3e57282579b2a
Andrew Bunac <andrew.bunac@telusinternational.com>
2024-04-25 21:54:04 +0800

    41. Caching Files in Github Actions (7)

--- sum.test.js
+++ sum.test.js
@@ -6,7 +6,7 @@ test('adds 1 + 2 to equal 3', () => {
 
 /// create another test  case that will test if the sum of 2 adn 4 is equal to 6
 test('adds 2 + 4 to equal 6', () => {
-    expect(sum(2,4)).toBe(5);
+    expect(sum(2,4)).toBe(6);
 });
 
 // Run npx jest --coverage to run test results and see the coverage of the test
